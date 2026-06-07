009b1911b1f6c74f9674f0ce78cb8b158dc143d8
Cássio Nunes <cassioczo@hotmail.com>
2026-06-06 23:19:10 -0300

    Aula 302 - App Orçamento Pessoal - Classe BD e índices dinâmicos

--- ES6/app1/app.js
+++ ES6/app1/app.js
@@ -9,8 +9,31 @@ class Despesa {
     }
 }
 
-function cadastrarDespesa() {
+class Bd {
+    constructor() {
+        let id = localStorage.getItem('id');
 
+        if (id === null) {
+            localStorage.setItem('id', 0);
+        }
+    }
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id');
+        return parseInt(proximoId) + 1;
+    }
+
+    gravar(d) {
+        let id = this.getProximoId();
+
+        localStorage.setItem(id, JSON.stringify(d));
+        localStorage.setItem('id', id);
+    }
+}
+
+let bd = new Bd();
+
+function cadastrarDespesa() {
     let ano = document.getElementById('ano');
     let mes = document.getElementById('mes');
     let dia = document.getElementById('dia');
@@ -27,9 +50,5 @@ function cadastrarDespesa() {
         valor.value
     );
 
-    gravar(despesa);
-}
-
-function gravar(d) {
-    localStorage.setItem('despesa', JSON.stringify(d));
+    bd.gravar(despesa);
 }
