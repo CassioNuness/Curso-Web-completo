65a28ecfc1590ac478c52a9def4045a29b0b1523
Cássio Nunes <cassioczo@hotmail.com>
2026-06-13 15:05:19 -0300

    aula 327 - Comentarios

---
+++ php/comentarios.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Curso PHP</title>
+
+	</head>
+
+	<body>
+        
+        <?php
+        
+        // Comentário de uma linha
+        echo 'Comando echo<br />';
+
+        # Comentário de uma linha
+        echo print("Comando print<hr />");
+
+        /*
+
+        Comentário de múltiplas linhas.
+        Este tipo de comentário permite que varias
+        linhas sejam comentadadas ao mesmo tempo.
+
+        */
+        
+        ?>
+        
+        
+	</body>	
+
+</html>
