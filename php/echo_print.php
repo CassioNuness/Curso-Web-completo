e940be56872bbb440603818d16053073027e555e
Cássio Nunes <cassioczo@hotmail.com>
2026-06-13 14:37:13 -0300

    aula 326 - Saida de dados com echo() e print()

---
+++ php/echo_print.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Curso PHP</title>
+
+	</head>
+
+	<body>
+        
+        <?php
+        
+        // Comando echo e print são utilizados para imprimir algo na tela, ambos tem a mesma função, porém o echo é mais utilizado.
+        echo 'Comando echo<br />';
+
+        // O comando print e uma função, ou seja, tem um valor de retorno, diferente do echo que é uma construção da linguagem, ou seja, não tem valor de retorno.
+        echo print("Comando print<hr />");
+        
+        ?>
+        
+	</body>	
+
+</html>
