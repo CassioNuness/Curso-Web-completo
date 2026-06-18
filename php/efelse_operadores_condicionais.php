051f4fbeadb9a2be8cc7b8066c4c7f32084e148b
Cássio Nunes <cassioczo@hotmail.com>
2026-06-18 19:08:11 -0300

    aula 336 - if/else parte 2

---
+++ php/efelse_operadores_condicionais.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Curso PHP</title>
+
+	</head>
+
+	<body>
+        
+        <?php
+
+        //==
+        //===
+        //!= ou <>
+        //!==
+        // <
+        // >
+        // <=
+        // >=
+
+        if(25 >= 15) {
+            echo 'verdadeiro';
+        }else {
+            echo 'falso';
+        }
+			
+        ?>
+
+
+        
+	</body>	
+
+</html>
