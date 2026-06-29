3bbfa021026645203020674b4bc1a2688daac3d8
Cássio Nunes <cassioczo@hotmail.com>
2026-06-28 21:08:42 -0300

    aula 348  Operações aritméticas na atribuição de valores

---
+++ php/operadores_atribuicao_aritimeticos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Curso PHP</title>
+
+</head>
+
+<body>
+
+    <?php
+
+    $x = 10;
+    $y = 8;
+
+    //$x = $x + 5;
+    $x %= $y;
+
+    echo "O valor de " . ($x) . " é";
+
+    ?>
+
+
+
+</body>
+
+</html>
