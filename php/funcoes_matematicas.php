1266c9224d7a0267cc5926bade23d5ea099e7c86
Cássio Nunes <cassioczo@hotmail.com>
2026-06-30 22:53:26 -0300

    aula 354 - funções matemáticas

---
+++ php/funcoes_matematicas.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Curso PHP</title>
+
+</head>
+
+<body>
+
+
+    <?php
+
+    $num = 9.999;
+    echo ceil($num); //arredonda pra cima
+    echo floor($num); //arredonda pra baixo
+    echo round($num); //arredonda com base na fraçao
+    echo rand($num); //gerar um valor aleatório de 0 até randamax
+    echo '<br />';
+    echo sqrt(9);
+
+    ?>
+
+
+
+</body>
+
+</html>
