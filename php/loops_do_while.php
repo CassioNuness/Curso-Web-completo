319b0076d8fe3b64273998bea19cc1c5bb92240f
Cássio Nunes <cassioczo@hotmail.com>
2026-07-04 19:59:31 -0300

    aula 364 - Loop Do While

---
+++ php/loops_do_while.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Curso PHP</title>
+
+</head>
+
+<body>
+
+
+    <?php
+
+        $x = 1;
+
+        do {
+            echo "X = $x <br/>";
+
+            $x++; //criterio de parada do loop
+
+        } while ($x <= 9);
+
+    ?>
+
+</body>
+
+</html>
