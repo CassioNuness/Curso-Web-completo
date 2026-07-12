b7d2ac8b32ec78accadcb0b8f06d7d5bca7ce5fa
Cássio Nunes <cassioczo@hotmail.com>
2026-07-12 20:16:30 -0300

    aula 389 - Getters e Setters mágicos (overloanding de atributos e métodos)

--- php_oo/oo_pilar_abstracao.php
+++ php_oo/oo_pilar_abstracao.php
@@ -7,8 +7,19 @@ class Funcionario {
     public $nome = null;
     public $telefone = null;
     public $numFilhos = null;
+    public $cargo = null;
+    public $salario = null;
 
-    //getters e setters
+    //getters e setters (overloading / sobrecarga)
+    function __set($atributo, $valor) {
+        $this->$atributo = $valor;
+    }
+
+    function __get($atributo) {
+        return $this->$atributo;
+    }
+
+    /*
     function setNome($nome) {
         $this->nome = $nome;
     }
@@ -32,6 +43,7 @@ class Funcionario {
     function getTelefone() {
         return $this->telefone;
     }
+    */
 
     //métodos
     function resumirCadFunc() {
@@ -45,6 +57,7 @@ class Funcionario {
 
 }
 
+/*
 $y = new Funcionario();
 $y->setNome('José');
 $y->setNumFilhos(2);
@@ -67,5 +80,43 @@ echo $x->getNome() .
      $x->getNumFilhos() .
      ' filho(s) e meu número é: ' .
      $x->getTelefone();
+
+*/
+
+$y = new Funcionario();
+$y->__set('nome', 'José');
+$y->__set('numFilhos', 2);
+$y->__set('telefone', '31 9 9300 6412');
+$y->__set('cargo', 'Programador jr');
+$y->__set('salario', '3000');
+// echo $y->resumirCadFunc();
+echo $y->__get('nome') .
+     ' possui ' .
+     $y->__get('numFilhos') .
+     ' filho(s) e o número é: ' .
+     $y->__get('telefone') . 
+     ' e trabalha como: ' .
+     $y->__get('cargo') .
+     ' e o salário que recebe é: ' .
+     $y->__get('salario');
+
+echo '<hr />';
+
+$x = new Funcionario();
+$x->__set('nome', 'Maria');
+$x->__set('numFilhos', 0);
+$x->__set('telefone', '31 9 9301 6522');
+$x->__set('cargo', 'Professor(a)');
+$x->__set('salario', '4500');
+// echo $y->resumirCadFunc();
+echo $x->__get('nome') .
+     ' possui ' .
+     $x->__get('numFilhos') .
+     ' filho(s) e o número é: ' .
+     $x->__get('telefone') . 
+     ' e trabalha como: ' .
+     $x->__get('cargo') .
+     ' e o salário que recebe é: ' .
+     $x->__get('salario');
 
 ?>
