a37a8b94ff16c45032a21dfb78e5086b56ba75a3
Cássio Nunes <cassioczo@hotmail.com>
2026-07-12 19:28:25 -0300

    aula 389 - Getters e Setters

--- php_oo/oo_pilar_abstracao.php
+++ php_oo/oo_pilar_abstracao.php
@@ -4,9 +4,34 @@
 class Funcionario {
 
     //atributos
-    public $nome = 'José';
-    public $telefone = '31 9 9300-2513';
-    public $numFilhos = 2;
+    public $nome = null;
+    public $telefone = null;
+    public $numFilhos = null;
+
+    //getters e setters
+    function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    function setNumFilhos($numFilhos) {
+        $this->numFilhos = $numFilhos;
+    }
+
+    function setTelefone($telefone) {
+        $this->telefone = $telefone;
+    }
+
+    function getNome() {
+        return $this->nome;
+    }
+
+    function getNumFilhos() {
+        return $this->numFilhos;
+    }
+
+    function getTelefone() {
+        return $this->telefone;
+    }
 
     //métodos
     function resumirCadFunc() {
@@ -21,21 +46,26 @@ class Funcionario {
 }
 
 $y = new Funcionario();
-echo $y->resumirCadFunc();
-
-echo '<br />';
-
-$y->modificarNumFilhos(3);
-echo $y->resumirCadFunc();
+$y->setNome('José');
+$y->setNumFilhos(2);
+$y->setTelefone('31 9 9300 6412');
+// echo $y->resumirCadFunc();
+echo $y->getNome() .
+     ' possui ' .
+     $y->getNumFilhos() .
+     ' filho(s) e meu número é: ' .
+     $y->getTelefone();
 
 echo '<hr />';
 
-$x = new Funcionario();
-echo $x->resumirCadFunc();
-
-echo '<br />';
-
-$x->modificarNumFilhos(1);
-echo $x->resumirCadFunc();
+$x = new funcionario();
+$x->setNome('Maria');
+$x->setNumFilhos(0);
+$x->setTelefone('31 9 9300 0209');
+echo $x->getNome() .
+     ' possui ' .
+     $x->getNumFilhos() .
+     ' filho(s) e meu número é: ' .
+     $x->getTelefone();
 
 ?>
