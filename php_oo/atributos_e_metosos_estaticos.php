893ca714ed84d82bbd5b8e927210097f84809a28
Cássio Nunes <cassioczo@hotmail.com>
2026-07-16 14:38:32 -0300

    aula 396 - OO - Atributos e métodos estáticos

---
+++ php_oo/atributos_e_metosos_estaticos.php
@@ -0,0 +1,24 @@
+<?php
+
+    class Exemplo {
+        public static $atributo1 = 'Eu sou um atributo estático';
+        public $atributo2 = 'Eu sou um atributo normal';
+
+        public static function metodo1() {
+            echo 'Eu sou um atributo estático';
+        }
+
+        public function metodo2() {
+            echo 'Eu sou um atributo normal';
+        }
+
+    }
+
+    // echo Exemplo::$atributo1;
+    // echo '<br />';
+    // Exemplo::metodo1();
+
+    $x = new Exemplo();
+    echo $x->atributo2;
+
+?>
